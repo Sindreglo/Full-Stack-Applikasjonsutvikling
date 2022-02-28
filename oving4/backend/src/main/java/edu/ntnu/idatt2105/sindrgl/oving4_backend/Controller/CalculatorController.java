64f78082735c208cee52e2ff30040f072910720f
sindreglomnes <sindre.glomnes@gmail.com>
2022-02-28 19:32:22 +0100

    Logger

--- oving4/backend/src/main/java/edu/ntnu/idatt2105/sindrgl/oving4_backend/Controller/CalculatorController.java
+++ oving4/backend/src/main/java/edu/ntnu/idatt2105/sindrgl/oving4_backend/Controller/CalculatorController.java
@@ -3,6 +3,8 @@ package edu.ntnu.idatt2105.sindrgl.oving4_backend.Controller;
 import edu.ntnu.idatt2105.sindrgl.oving4_backend.Model.Calculation;
 import edu.ntnu.idatt2105.sindrgl.oving4_backend.Service.CalculatorService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,8 +16,17 @@ public class CalculatorController {
     @Autowired
     private CalculatorService calculatorService;
 
+    Logger logger = LoggerFactory.getLogger(CalculatorController.class);
+
     @PostMapping("/calculation")
     public Calculation postCalculation(@RequestBody Calculation calculation){
+
+        logger.trace("ermahgerd trers");
+        logger.debug("ermahgerd derberg");
+        logger.info("ermahgerd ernferh");
+        logger.warn("ermahgerd wern");
+        logger.error("ermahgerd ehrrerr");
+
         return calculatorService.calculate(calculation);
     }
 }
